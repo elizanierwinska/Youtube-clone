deb78b5f2203e760c81b401b60666e5cd8ede020
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-07 00:35:37 +0100

    feature: create left sidebar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Video from './Pages/Video/Video';
 
+// Implement paths by creating Routes element and writing Route element within it
+// In Route element specify path you want and the component
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />}></Route>
+      </Routes>
     </div>
   )
 }
